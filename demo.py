455b99f7bc0785820f6217dff2046cf19f9e5c50
AndreiChertkov <andre.chertkov@gmail.com>
2023-07-17 16:23:42 +0300

    Big update for the main demo script

--- demo.py
+++ demo.py
@@ -1,39 +1,19 @@
-import subprocess
+"""Demo script for teneva_bm.
+
+Call it as "python demo.py" to run demo for all existing benchmarks or as
+"python demo.py bm_<name_of_collection>_<name_of_benchmark>" to run an example
+for a specific benchmark "<name_of_benchmark>" from the collection (folder)
+"<name_of_collection" (e.g., "python demo.py bm_qubo_knap_amba"). Note that the
+code for the demo run is at the end of the corresponding benchmark file in the
+section "if __name__ == '__main__':".
+
+"""
 import sys
 
 
-def demo(bm_use=None):
-    # Extract the file names of all benchmarks from the "__init__.py" file:
-    with open('teneva_bm/__init__.py', encoding='utf-8') as f:
-        lines = f.readlines()
-    bms = []
-    modules = []
-    for l in lines:
-        if 'import' in l and l[0] != '#':
-            bm_full = l.split('from .')[1].split(' import')[0]
-            if not '.' in bm_full:
-                continue
-
-            bms.append(bm_full.split('.')[1])
-            modules.append(bm_full.split('.')[0])
-
-    print(f'Full list of benchmark files (total: {len(bms)}):')
-    print('>>>>', '; '.join(bms))
-    print('\n\n')
-
-    # Run the benchmark python file as the direct console call:
-    for bm, module in zip(bms, modules):
-        if bm_use and bm_use != bm:
-            continue
-
-        out = subprocess.getoutput(f'python teneva_bm/{module}/{bm}.py')
-        print(out + '\n\n\n')
-        if 'Traceback' in out:
-            '\n\n<----- ERROR !!! Break.\n\n'
-            return
+from teneva_bm import teneva_bm_demo
 
 
 if __name__ == '__main__':
     bm_use = sys.argv[1] if len(sys.argv) > 1 else None
-
-    demo(bm_use)
+    teneva_bm_demo(bm_use, with_info=(bm_use is None))
---
+++ teneva_bm/teneva_bm_demo.py
@@ -0,0 +1,87 @@
+import os
+import subprocess
+
+
+def teneva_bm_demo(bm_use=None, with_info=True):
+    """Demo script for teneva_bm.
+
+    Run demo for all existing benchmarks (if "bm_use" is None) or for a
+    specific benchmark (if "bm_use" is provided; e.g., "bm_qubo_knap_amba"). If
+    the flag "with_info" is set, then the stats for existing collections and
+    benchmarks will be also printed. Note that the code for the demo run of
+    benchmarks should be at the end of the corresponding benchmark file in the
+    section "if __name__ == '__main__':".
+
+    """
+    bms = {}
+    for cl in _find_cl_all():
+        bms[cl] = _find_bm_all(cl)
+
+    if with_info:
+        _info(bms)
+
+    _run_all(bms, bm_use)
+
+
+def _find_bm_all(cl):
+    """Collect all existing benchmark names for the provided collection name."""
+    bms = []
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(f'{here}/{cl}/__init__.py', encoding='utf-8') as f:
+        lines = f.readlines()
+    for l in lines:
+        if 'from .' in l and ' import ' in l and l[0] != '#':
+            bms.append(l.split('from .')[1].split(' import')[0])
+    return bms
+
+
+def _find_cl_all():
+    """Collect all existing collection names."""
+    cls = []
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(f'{here}/__init__.py', encoding='utf-8') as f:
+        lines = f.readlines()
+    for l in lines:
+        if 'from .' in l and ' import *' in l and l[0] != '#':
+            cls.append(l.split('from .')[1].split(' import *')[0])
+    return cls
+
+
+def _info(bms):
+    """Present the stats for existing collections and benchmarks."""
+    text = '\n\n\n' + '-' * 70 + '\n'
+    text += '-' * 19 + ' Benchmarks library (teneva_bm) ' + '-' * 19 + '\n'
+    text += '-' * 70 + '\n\n'
+
+    for cl, bm_list in bms.items():
+        count = len(bm_list)
+        text += f'--> {cl}' + ' ' * max(0, 10-len(cl))
+        text += f': {count:-4d} benchmarks'
+        for i in range(count):
+            if i == 0 or i % 3 == 0:
+                text += '\n' + ' ' * 14 + '> '
+            text += str(bm_list[i])
+            if i < count - 1:
+                text += ', '
+        text += '\n'
+
+    text += '\n' + '=' * 70 + '\n\n\n\n'
+
+    print(text)
+
+
+def _run_all(bms, bm_use=None):
+    here = os.path.abspath(os.path.dirname(__file__))
+
+    for cl, bm_list in bms.items():
+        for bm in bm_list:
+            if bm_use and bm_use != bm:
+                continue
+
+            path = f'{here}/{cl}/{bm}.py'
+            out = subprocess.getoutput(f'python {path}')
+            print(out + '\n\n')
+
+            if 'Traceback' in out:
+                '\n\n<----- ERROR !!! Break.\n\n'
+                return
