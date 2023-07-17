53e6c0af63eab287a1a10183eafba4cc4f873319
AndreiChertkov <andre.chertkov@gmail.com>
2023-07-17 16:50:05 +0300

    Update for BmFuncQing

--- teneva_bm/func/bm_func_qing.py
+++ teneva_bm/func/bm_func_qing.py
@@ -8,12 +8,15 @@ from teneva_bm import Bm
 DESC = """
     Analytical Qing function (continuous).
     The dimension and mode size may be any (default are d=50, n=15).
+    Default grid limits are [0, +500] (note that we limit this function
+    to this domain instead of often used [-500, 500] to make sure it has
+    a single global minimum); the exact global minimum
+    is known: x_i = \sqrt{i} (i = 1, ..., d), y = 0.
     See the work Momin Jamil, Xin-She Yang. "A literature survey of
     benchmark functions for global optimization problems". Journal of
     Mathematical Modelling and Numerical Optimisation 2013; 4:150-194
     ("98. Qing Function"; Continuous, Differentiable, Separable
-    Scalable, Multimodal). Note that we limit this function to the
-    [0, 500] domain to make sure it has a single global minimum.
+    Scalable, Multimodal).
 """
 
 
@@ -23,7 +26,9 @@ class BmFuncQing(Bm):
 
         self.set_grid(0., +500.)
 
-        self.set_min(y=0., x=np.sqrt(np.arange(1, self.d+1)))
+        self.set_min(
+            x=np.sqrt(np.arange(1, self.d+1)),
+            y=0.)
 
         self.with_cores = True
 
@@ -50,20 +55,16 @@ if __name__ == '__main__':
     bm = BmFuncQing().prep()
     print(bm.info())
 
-    text = 'Range of y for 10K random samples : '
-    bm.build_trn(1.E+4)
-    text += f'[{np.min(bm.y_trn):-10.3e},'
-    text += f' {np.max(bm.y_trn):-10.3e}] '
-    text += f'(avg: {np.mean(bm.y_trn):-10.3e})'
-    print(text)
+    I_trn, y_trn = bm.build_trn(1.E+4)
+    print(bm.info_history())
 
-    text = 'Value at a random multi-index     :  '
+    text = 'Value at a random multi-index            :  '
     i = [np.random.choice(k) for k in bm.n]
     y = bm[i]
     text += f'{y:-10.3e}'
     print(text)
 
-    text = 'Value at 3 random multi-indices   :  '
+    text = 'Value at 3 random multi-indices          :  '
     i1 = [np.random.choice(k) for k in bm.n]
     i2 = [np.random.choice(k) for k in bm.n]
     i3 = [np.random.choice(k) for k in bm.n]
@@ -72,14 +73,14 @@ if __name__ == '__main__':
     text += '; '.join([f'{y_cur:-10.3e}' for y_cur in y])
     print(text)
 
-    text = 'TT-cores accuracy on train data   :  '
+    text = 'TT-cores accuracy on train data          :  '
     Y = bm.build_cores()
-    e = teneva.accuracy_on_data(Y, bm.I_trn, bm.y_trn)
+    e = teneva.accuracy_on_data(Y, I_trn, y_trn)
     text += f'{e:-10.1e}'
     print(text)
 
-    text = 'Value at minimum (real vs calc)   :  '
+    text = 'Value at the minimum (real vs calc)      :  '
     y_real = bm.y_min_real
     y_calc = bm(bm.x_min_real)
-    text += f'{y_real:-10.3e}/ {y_calc:-10.3e}'
+    text += f'{y_real:-10.3e}       /      {y_calc:-10.3e}'
     print(text)
