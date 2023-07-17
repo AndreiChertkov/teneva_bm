9c3492f10354a899d62c671299344fc749621f83
AndreiChertkov <andre.chertkov@gmail.com>
2023-07-17 18:09:27 +0300

    Update for BmQuboMaxcut

--- teneva_bm/qubo/bm_qubo_maxcut.py
+++ teneva_bm/qubo/bm_qubo_maxcut.py
@@ -34,19 +34,26 @@ class BmQuboMaxcut(Bm):
         if not self.is_n_equal or self.n[0] != 2:
             self.set_err('Mode size (n) should be "2"')
         if not with_networkx:
-            self.set_err('Need "networkx" module')
+            msg = 'Need "networkx" module. For installation please run '
+            msg += '"pip install networkx==3.0"'
+            self.set_err(msg)
         if not with_qubogen:
-            self.set_err('Need "qubogen" module')
+            msg = 'Need "qubogen" module. For installation please run '
+            msg += '"pip install qubogen==0.1.1"'
+            self.set_err(msg)
 
     @property
     def is_tens(self):
         return True
 
     def prep(self):
-        graph = nx.fast_gnp_random_graph(n=self.d, p=self.opt_prob_con,
-            seed=self.opt_seed)
+        self.check_err()
+
+        graph = nx.fast_gnp_random_graph(n=self.d,
+            p=self.opt_prob_con, seed=self.opt_seed)
         edges = np.array(list([list(e) for e in graph.edges]))
         n_nodes = len(np.unique(np.array(edges).flatten()))
+
         g = qubogen.Graph(edges=edges, n_nodes=n_nodes)
         self.bm_Q = qubogen.qubo_max_cut(g)
 
@@ -54,7 +61,7 @@ class BmQuboMaxcut(Bm):
         return self
 
     def set_opts(self, prob_con=0.5, seed=42):
-        """Setting options specific to this benchmark.
+        """Setting options specific to the benchmark.
 
         Args:
             prob_con (float): probability of the connection in the graph.
@@ -74,20 +81,16 @@ if __name__ == '__main__':
     bm = BmQuboMaxcut().prep()
     print(bm.info())
 
-    text = 'Range of y for 10K random samples : '
-    bm.build_trn(1.E+4)
-    text += f'[{np.min(bm.y_trn):-10.3e},'
-    text += f' {np.max(bm.y_trn):-10.3e}] '
-    text += f'(avg: {np.mean(bm.y_trn):-10.3e})'
-    print(text)
+    I_trn, y_trn = bm.build_trn(1.E+4)
+    print(bm.info_history())
 
-    text = 'Value at a random multi-index     :  '
+    text = 'Value at a random multi-index            :  '
     i = [np.random.choice(k) for k in bm.n]
     y = bm[i]
     text += f'{y:-10.3e}'
     print(text)
 
-    text = 'Value at 3 random multi-indices   :  '
+    text = 'Value at 3 random multi-indices          :  '
     i1 = [np.random.choice(k) for k in bm.n]
     i2 = [np.random.choice(k) for k in bm.n]
     i3 = [np.random.choice(k) for k in bm.n]
