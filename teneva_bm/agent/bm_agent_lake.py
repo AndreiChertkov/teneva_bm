137b9d668e9d2a0602581f2e2c5323da0242c50d
AndreiChertkov <andre.chertkov@gmail.com>
2023-07-30 21:21:02 +0300

    Big update for BmAgentLake

--- teneva_bm/agent/bm_agent_lake.py
+++ teneva_bm/agent/bm_agent_lake.py
@@ -23,42 +23,66 @@ class BmAgentLake(Agent):
     HOLE = 'H'
     GOAL = 'G'
 
-    def map(size, rand, holes_min, holes_max, p=0.8):
-        if not with_gym:
-            return [], []
+    def holes(map):
+        size = len(map)
+        holes = []
+        for i in range(size):
+            for j in range(size):
+                if map[i][j] == BmAgentLake.HOLE:
+                    holes.append(np.array([i, j], dtype=int))
+        return holes
 
+    def map(size=5, prob_hole=0.4, seed=42):
         map = []
+        rand = np.random.default_rng(seed) if isinstance(seed, int) else seed
         while len(map) == 0 or not Agent.frozen_lake.is_valid(map, size):
-            p = min(1., p)
             map = rand.choice([BmAgentLake.FROZ, BmAgentLake.HOLE],
-                (size, size), p=[p, 1-p])
+                (size, size), p=[1-prob_hole, prob_hole])
             map[0, 0] = BmAgentLake.INIT
             map[-1, -1] = BmAgentLake.GOAL
-
-            holes = []
-            for i in range(size):
-                for j in range(size):
-                    if map[i, j] == BmAgentLake.HOLE:
-                        holes.append(np.array([i, j], dtype=int))
-
-            if len(holes) < holes_min or len(holes) > holes_max:
-                map = []
-
-        return [''.join(x) for x in map], holes
+        return [''.join(x) for x in map]
 
     def __init__(self, d=None, n=4, name='AgentLake', desc=DESC,
-                 steps=100, size=5, holes_min=10, holes_max=20,
-                 policy='direct', with_state_ext=False):
-        super().__init__(d, n, name, desc, steps, policy_name)
+                 steps=100, policy='direct',
+                 size=5, prob_hole=0.4, with_state_ext=False):
+        super().__init__(d, n, name, desc, steps, policy)
 
         self._size = size
-        self._holes_min = holes_min
-        self._holes_max = holes_max
+        self._prob_hole = prob_hole
         self._with_state_ext = with_state_ext
 
     @property
     def is_func(self):
         return False
+
+    @property
+    def _a_ac(self):
+        return None
+
+    @property
+    def _a_st(self):
+        return None
+
+    @property
+    def _b_ac(self):
+        return None
+
+    @property
+    def _b_st(self):
+        return None
+
+    @property
+    def _d_ac(self):
+        return 1
+
+    @property
+    def _d_st(self):
+        if self._with_state_ext:
+            # State includes positions of the holes, goal and agent
+            return len(self._holes) * 2 + 2 + 2
+        else:
+            # State includes only current position of the agent
+            return 2
 
     @property
     def _is_state_done(self):
@@ -78,113 +102,103 @@ class BmAgentLake(Agent):
 
     @property
     def _is_state_hole(self):
-        for hole in self.holes:
+        for hole in self._holes:
             if hole[0] == self._state[0] and hole[1] == self._state[1]:
                 return True
         return False
 
     @property
     def _is_state_same(self):
-        if self.step == 0:
+        if self._step_prev is None:
             return False
-        if self._state[0] != self._state_prev[0]:
+        if self._state[0] != self._step_prev[0]:
             return False
-        if self._state[1] != self._state_prev[1]:
+        if self._state[1] != self._step_prev[1]:
             return False
         return True
 
     @property
-    def _state_out(self):
-        """The output state for the policy."""
-        if not self._with_state_ext:
-            return self._state
+    def _n_ac(self):
+        return np.array([self._env.action_space.n] * self._d_ac, dtype=int)
 
-        state = []
-        for hole in self.holes:
-            state.extend(list(hole))
-        state.extend(list(self._state_goal))
-        state.extend(list(self._state))
+    @property
+    def _n_st(self):
+        return np.array([self._env.observation_space.n] * self._d_st, dtype=int)
 
-        return np.array(state, dtype=int)
+    @property
+    def _step_prev(self):
+        if len(self._states) == 0:
+            return None
+        elif len(self._states) == 1:
+            return self._state0
+        else:
+            return self._states[-2]
+
+    def get_config(self):
+        conf = super().get_config()
+        conf['_size'] = self._size
+        conf['_prob_hole'] = self._prob_hole
+        conf['_with_state_ext'] = self._with_state_ext
+        return conf
+
+    def info(self, footer=''):
+        text = ''
+
+        text += 'Size of the lake                         : '
+        v = self._size
+        text += f'{v}x{v}\n'
+
+        text += 'Probability of the hole                  : '
+        v = self._prob_hole
+        text += f'{v}\n'
+
+        text += 'State is extended                        : '
+        v = 'YES' if self._with_state_ext else 'no'
+        text += f'{v}\n'
+
+        return super().info(text+footer)
 
     def prep_bm(self, policy=None):
-        map, self._holes = BmAgentLake.map(self._size, self.rand,
-            self._holes_min, self._holes_max)
+        self._map = BmAgentLake.map(self._size, self._prob_hole, self.rand)
+        self._holes = BmAgentLake.holes(self._map)
 
-        env = Agent.env_build('FrozenLake-v1', desc=map, is_slippery=False)
+        env = Agent.make('FrozenLake-v1', desc=self._map, is_slippery=False)
 
         self._state_goal = self._discretize(self._size*self._size-1)
 
-        if self._with_state_ext:
-            # State includes positions of the holes, goal and agent
-            d_st = len(self._holes) * 2 + 2 + 2
-        else:
-            # State includes only current position of the agent
-            d_st = 2
+        return super().prep_bm(env)
 
-        d_ac = 1
-        self._n_st = np.array([env.observation_space.n] * d_st, dtype=int)
-        self._n_ac = np.array([env.action_space.n] * d_ac, dtype=int)
-
-        return super().prep_bm(env, policy, d_st, d_ac)
-
-    def render000(self, actions, fpath=None):
-        """Generate the video for agent movements with given actions."""
-        self._reset()
-
-        if not fpath:
-            fpath = f'AgentLake_size-{self.size}_seed-{self.seed}.avi'
-        if not fpath.endswith('.avi'):
-            fpath += '.avi'
-        if os.path.dirname(fpath):
-            os.makedirs(os.path.dirname(fpath), exist_ok=True)
-
-
-        frame = self._env.render()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.resize(frame,(512, 512))
-
-        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        out = cv2.VideoWriter(fpath, fourcc, 5.0, (512, 512))
-
-        ret = out.write(frame)
-
-        for a in actions:
-            state, reward, done, truncated, info = self._env.step(int(a))
-
-            frame = self._env.render()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame = cv2.resize(frame,(512, 512))
-
-            ret = out.write(frame)
-
-            if done:
-                break
-
-        out.release()
-
-    def show(self, fpath=None, i=None, best=True):
-        frame = self._env.render()
-        plt.imshow(frame)
-        plt.axis('off')
-
-        fpath = self.path_build(fpath, 'png')
-        plt.savefig(fpath, bbox_inches='tight') if fpath else plt.show()
+    def render(self, fpath=None, i=None, best=True, fps=5.):
+        return super().render(fpath, i, best, fps)
 
     def _discretize(self, state):
         x, y = (state // self._size, state % self._size)
         return np.array([x, y], dtype=int)
 
+    def _parse_action_gym(self, action):
+        return action[0]
+
     def _parse_state(self, state):
         return self._discretize(state)
 
-    def _parse_reward(self, reward=0., eps=1.E-4):
-        dist = np.linalg.norm(self._state_goal - self._state)
+    def _parse_state_policy(self, state):
+        if not self._with_state_ext:
+            return state
+
+        state_ext = []
+        for hole in self._holes:
+            state_ext.extend(list(hole))
+        state_ext.extend(list(self._state_goal))
+        state_ext.extend(list(state))
+        return state_ext
+
+    def _parse_reward(self, reward, eps=1.E-4):
+        dist = np.linalg.norm(self._state - self._state_goal)
         reward += 1. / (dist + eps)
         # reward += 1000 if self._is_state_goal else 0
         # reward -= 1000 if self._is_state_hole else 0
-        # reward -= 10 if self._is_state_same else 0
-        # reward -= self.step
+        # reward -= 10 if self._is_state_same(state) else 0
+        # reward -= self._step
         return reward
 
 
