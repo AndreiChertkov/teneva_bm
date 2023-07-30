28a6a9280553e4136733ab7bc709dc4b4ea50905
AndreiChertkov <andre.chertkov@gmail.com>
2023-07-30 19:30:58 +0300

    Big update for Agent and Policy classes

--- teneva_bm/agent/agent.py
+++ teneva_bm/agent/agent.py
@@ -1,5 +1,5 @@
+import matplotlib.pyplot as plt
 import numpy as np
-import os
 
 
 try:
@@ -11,10 +11,19 @@ except Exception as e:
 
 try:
     import gym
-    with_gym = True
+    gym.logger.set_level(40)
     np.bool8 = bool
+    from gym.envs.toy_text import frozen_lake as gym_frozen_lake
+    with_gym = True
 except Exception as e:
     with_gym = False
+
+
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning,
+    module=r"importlib\.")
+warnings.filterwarnings('ignore', category=DeprecationWarning,
+    module=r"ipkernel")
 
 
 from teneva_bm import Bm
@@ -27,8 +36,10 @@ class Agent(Bm):
         if with_gym:
             return gym.make(name, render_mode='rgb_array', **args)
 
+    frozen_lake = gym_frozen_lake if with_gym else None
+
     def __init__(self, d=None, n=3, name='Agent-abstract-class', desc='',
-                 steps=1000, policy_name='toeplitz'):
+                 steps=1000, policy='toeplitz'):
         super().__init__(None, None, name, desc)
         self._n_raw = n # We'll set it later in "prep_bm" method
 
@@ -45,46 +56,90 @@ class Agent(Bm):
             self.set_err(msg)
 
         self._steps = steps
-        self._policy_name = policy_name
+
+        if isinstance(policy, str):
+            self._policy_name = policy
+
+            if self._policy_name == 'direct':
+                self._policy = Policy()
+            elif self._policy_name == 'toeplitz':
+                self._policy = PolicyToeplitz()
+            else:
+                raise ValueError('Invalid policy name')
+
+        else:
+            self._policy_name = policy.name
+
+            self._policy = policy
 
         self._with_render = False
 
     @property
     def identity(self):
-        return ['_policy_name', 'n', '_steps']
+        return ['_steps', '_policy_name', 'n']
 
     @property
     def is_func(self):
-        return self._policy.is_func
+        return True
 
     @property
     def is_opti_max(self):
         return True
 
     @property
+    def _d_ac(self):
+        return len(self._env.action_space.low)
+
+    @property
+    def _d_st(self):
+        return len(self._env.observation_space.low)
+
+    @property
+    def _a_ac(self):
+        return np.array(self._env.action_space.low)
+
+    @property
+    def _a_st(self):
+        return np.array(self._env.observation_space.low)
+
+    @property
+    def _b_ac(self):
+        return np.array(self._env.action_space.high)
+
+    @property
+    def _b_st(self):
+        return np.array(self._env.observation_space.high)
+
+    @property
+    def _n_ac(self):
+        return None
+
+    @property
+    def _n_st(self):
+        return None
+
+    @property
     def _reward(self):
+        return self._rewards[-1] if len(self._rewards) else 0.
+
+    @property
+    def _reward_total(self):
         return np.sum(np.array(self._rewards)) if len(self._rewards) else 0.
 
     @property
     def _state(self):
-        return (self._states[-1] if len(self._states) else self._state0).copy()
+        return self._states[-1] if len(self._states) else self._state0
 
     def get_config(self):
         conf = super().get_config()
         conf['_steps'] = self._steps
         conf['_policy_name'] = self._policy_name
+        conf['_d_st'] = self._d_st
+        conf['_d_ac'] = self._d_ac
         return conf
 
     def info(self, footer=''):
         text = ''
-
-        text += 'Dimension of state space for agent       : '
-        v = self._d_st
-        text += f'{v}\n'
-
-        text += 'Dimension of action space for agent      : '
-        v = self._d_ac
-        text += f'{v}\n'
 
         text += 'Number of agent steps                    : '
         v = self._steps
@@ -94,18 +149,29 @@ class Agent(Bm):
         v = self._policy_name
         text += f'{v}\n'
 
+        text += 'Dimension of state space for agent       : '
+        v = self._d_st
+        text += f'{v}\n'
+
+        text += 'Dimension of action space for agent      : '
+        v = self._d_ac
+        text += f'{v}\n'
+
         return super().info(text+footer)
 
-    def prep_bm(self, env, policy=None, d_st=None, d_ac=None):
-        self._prep_env(env)
-        self._prep_space(d_st, d_ac)
-        self._prep_policy(policy)
+    def prep_bm(self, env):
+        if env is None:
+            raise ValueError('Environment is not set')
+        self._env = env
+
+        self._policy.prep(self._steps,
+            self._d_st, self._n_st, self._a_st, self._b_st,
+            self._d_ac, self._n_ac, self._a_ac, self._b_ac)
 
         self.set_dimension(self._policy.d)
         self.set_size(self._n_raw)
-        if self.is_func: # TODO !!!
-            self.set_grid(self._a_ac*self._steps, self._b_ac*self._steps)
-
+        if self.is_func:
+            self.set_grid(self._policy.a, self._policy.b)
         self._reset()
 
     def render(self, fpath=None, i=None, best=True):
@@ -124,62 +190,41 @@ class Agent(Bm):
             out.write(frame)
         out.release()
 
+    def show(self, fpath=None, i=None, best=True):
+        i, y = self.get_solution(i, best)
+
+        try:
+            frame = self._env.render()
+        except Exception as e:
+            self._wrn('Can not render agent for image generation')
+
+        plt.imshow(frame)
+        plt.axis('off')
+
+        fpath = self.path_build(fpath, 'png')
+        plt.savefig(fpath, bbox_inches='tight') if fpath else plt.show()
+
     def target(self, params):
         self._policy.set(params)
         self._run()
-        return self._reward
+        return self._reward_total
 
     def _parse_action(self, action):
         return action
-        if len(action) == 1:
-            action = action[0]
 
-    def _parse_reward(self, reward=0.):
+    def _parse_reward(self, reward, state):
         return reward
 
     def _parse_state(self, state):
         return state
 
-    def _prep_env(self, env):
-        if env is None:
-            raise ValueError('Environment is not set')
-        self._env = env
-
-    def _prep_policy(self, policy=None):
-        if policy is not None:
-            self._policy = policy
-        elif self._policy_name == 'none':
-            self._policy = Policy(self._d_st, self._d_ac, self._steps)
-        elif self._policy_name == 'toeplitz':
-            self._policy = PolicyToeplitz(self._d_st, self._d_ac, self._steps)
-        else:
-            raise ValueError('Policy is not set')
-
-    def _prep_space(self, d_st=None, d_ac=None):
-        if d_st:
-            self._d_st = d_st
-            self._a_st = None
-            self._b_st = None
-        else:
-            self._d_st = len(self._env.observation_space.low)
-            self._a_st = list(self._env.observation_space.low)
-            self._b_st = list(self._env.observation_space.high)
-
-        if d_ac:
-            self._d_ac = d_ac
-            self._a_ac = None
-            self._b_ac = None
-        else:
-            self._d_ac = len(self._env.action_space.low)
-            self._a_ac = list(self._env.action_space.low)
-            self._b_ac = list(self._env.action_space.high)
-
     def _reset(self):
+        self._policy.reset()
+
         state, info = self._env.reset(seed=self.seed)
 
         self._step = 0
         self._state0 = self._parse_state(state)
-        self._state_prev = None
         self._done = False
 
         self._actions = []
@@ -193,14 +238,13 @@ class Agent(Bm):
         for step in range(self._steps):
             self._step = step
 
-            action = self._policy(self._state, step)
+            action = self._policy(self._state)
             action = self._parse_action(action)
 
             state, reward, self._done = self._env.step(action)[:3]
             state = self._parse_state(state)
-            reward = self._parse_reward(reward)
+            reward = self._parse_reward(reward, state)
 
-            self._state_prev = self._state.copy()
             self._actions.append(action)
             self._rewards.append(reward)
             self._states.append(state)
--- teneva_bm/agent/policy.py
+++ teneva_bm/agent/policy.py
@@ -3,47 +3,81 @@ from scipy.linalg import toeplitz
 
 
 class Policy:
-    def __init__(self, d_st, d_ac, steps, is_func=True):
-        self.name = 'none'
+    def __init__(self, name='direct'):
+        self.name = name
 
-        self.d_st = d_st
-        self.d_ac = d_ac
-        self.steps = steps
-        self.is_func = is_func
+        self.reset()
+
+    @property
+    def a(self):
+        return np.array(list(self.a_ac) * self.steps)
+
+    @property
+    def b(self):
+        return np.array(list(self.b_ac) * self.steps)
 
     @property
     def d(self):
-        return self.steps * self.d_ac
+        return self.d_ac * self.steps
 
-    def __call__(self, state, step):
-        return self.act(state, step)
+    def __call__(self, state):
+        return self.act(state)
 
-    def act(self, state, step):
-        return self.actions[self.d_ac*step:self.d_ac*(step+1)]
+    def act(self, state):
+        action = self.actions[self.d_ac*self.step:self.d_ac*(self.step+1)]
+
+        self.step += 1
+        return action
+
+    def prep(self, steps=0, d_st=0, n_st=None, a_st=None, b_st=None,
+             d_ac=0, n_ac=None, a_ac=None, b_ac=None):
+        self.steps = steps
+
+        self.d_st = d_st
+        self.n_st = self._prep_opt(n_st, self.d_st, int)
+        self.a_st = self._prep_opt(a_st, self.d_st, float)
+        self.b_st = self._prep_opt(b_st, self.d_st, float)
+
+        self.d_ac = d_ac
+        self.n_ac = self._prep_opt(n_ac, self.d_ac, int)
+        self.a_ac = self._prep_opt(a_ac, self.d_ac, float)
+        self.b_ac = self._prep_opt(b_ac, self.d_ac, float)
+
+    def reset(self):
+        self.step = 0
 
     def set(self, params):
         self.actions = params.copy()
 
+    def _prep_opt(self, opt, d=None, kind=float):
+        if opt is None:
+            return None
+        if isinstance(opt, (int, float)):
+            if d is None or d <= 0:
+                raise ValueError('Invalid grid option')
+            opt = np.ones(d, dtype=kind) * kind(opt)
+        return np.asanyarray(opt, dtype=kind)
+
 
 class PolicyToeplitz(Policy):
-    def __init__(self, d_st, d_ac, steps, num_hidden=8):
-        super().__init__(d_st, d_ac, steps, is_func=False)
-
-        self.name = 'toeplitz'
+    def __init__(self, name='toeplitz', num_hidden=8):
+        super().__init__(name)
 
         self.d_in = num_hidden
 
-        self.d_w1 = self.d_st + self.d_in - 1
-        self.d_b1 = self.d_in
-        self.d_w2 = self.d_in * 2 - 1
-        self.d_b2 = self.d_in
-        self.d_w3 = self.d_ac + self.d_in - 1
+    @property
+    def a(self):
+        return np.array([-1.] * self.d)
+
+    @property
+    def b(self):
+        return np.array([+1.] * self.d)
 
     @property
     def d(self):
         return self.d_w1 + self.d_b1 + self.d_w2 + self.d_b2 + self.d_w3
 
-    def act(self, state, step):
+    def act(self, state):
         h1 = np.dot(self.W1, state) + self.b1
         z1 = self._activation(h1)
 
@@ -53,7 +87,17 @@ class PolicyToeplitz(Policy):
         out = np.dot(self.W3, z2)
         out = self._activation(out)
 
+        self.step += 1
         return out
+
+    def prep(self, *args, **kwargs):
+        super().prep(*args, **kwargs)
+
+        self.d_w1 = self.d_st + self.d_in - 1
+        self.d_b1 = self.d_in
+        self.d_w2 = self.d_in * 2 - 1
+        self.d_b2 = self.d_in
+        self.d_w3 = self.d_ac + self.d_in - 1
 
     def set(self, params):
         params = params.copy()
