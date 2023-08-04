39f308cf290532b8981229b98bf95d163141d196
AndreiChertkov <andre.chertkov@gmail.com>
2023-08-04 14:42:55 +0300

    Add BmAgentCheetah

--- teneva_bm/agent/__init__.py
+++ teneva_bm/agent/__init__.py
@@ -1,4 +1,5 @@
 from .bm_agent_ant import BmAgentAnt
+from .bm_agent_cheetah import BmAgentCheetah
 from .bm_agent_human import BmAgentHuman
 from .bm_agent_human_stand import BmAgentHumanStand
 from .bm_agent_lake import BmAgentLake
