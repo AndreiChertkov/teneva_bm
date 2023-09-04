2576d1c65746bd12838ccdb2b45a102ed74f16ff
ba <ba@atechlab.eu>
2023-09-04 20:04:58 +0300

    peps fix 3

--- teneva_bm/decomp/bm_decomp_peps.py
+++ teneva_bm/decomp/bm_decomp_peps.py
@@ -12,9 +12,9 @@ except Exception as e:
 
 
 def fill_diagonal(a, val):
-  assert a.ndim >= 2
-  i, j = jnp.diag_indices(min(a.shape[-2:]))
-  return a.at[..., i, j].set(val)
+    assert a.ndim >= 2
+    i, j = jnp.diag_indices(min(a.shape[-2:]))
+    return a.at[..., i, j].set(val)
 
 def prepare_array(key, dims):
     ones = jnp.array(dims)
